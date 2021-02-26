8571a34871e3f5d6620ec163637ff85e8c59497b
mi11km <mi11km.basket@gmail.com>
2021-02-26 23:14:16 +0900

    able not to notify during charging

--- internal/charge/charge.go
+++ internal/charge/charge.go
@@ -6,13 +6,13 @@ import (
 	"github.com/distatus/battery"
 )
 
-func Fetch(batteryCharges chan float64) {
+func Fetch(batteryInfo chan *battery.Battery) {
 	batteries, err := battery.GetAll()
 	if err != nil {
 		log.Printf("action=fetch battery info, err=%v\n", err)
 		return
 	}
 	for _, b := range batteries {
-		batteryCharges <- (b.Current / b.Full) * 100
+		batteryInfo <- b
 	}
 }
--- internal/confirmbattery/confirmbattery.go
+++ internal/confirmbattery/confirmbattery.go
@@ -8,18 +8,19 @@ import (
 	"sync"
 	"time"
 
+	"github.com/distatus/battery"
 	"github.com/wailsapp/wails"
 )
 
 type ConfirmBattery struct {
-	IsLoop         bool
-	BatteryCharges chan float64
-	runtime        *wails.Runtime
-	logger         *wails.CustomLogger
+	IsLoop      bool
+	BatteryInfo chan *battery.Battery
+	runtime     *wails.Runtime
+	logger      *wails.CustomLogger
 }
 
 func New() (*ConfirmBattery, error) {
-	return &ConfirmBattery{IsLoop: false, BatteryCharges: make(chan float64)}, nil
+	return &ConfirmBattery{IsLoop: false, BatteryInfo: make(chan *battery.Battery)}, nil
 }
 
 func (c *ConfirmBattery) WailsInit(runtime *wails.Runtime) error {
@@ -41,7 +42,7 @@ func (c *ConfirmBattery) Start(name, webhookURL string, confirmationInterval, no
 	go func() {
 		for {
 			wg.Add(1)
-			charge.Fetch(c.BatteryCharges)
+			charge.Fetch(c.BatteryInfo)
 			time.Sleep(time.Second * time.Duration(confirmationInterval))
 			if c.IsLoop {
 				break
@@ -49,19 +50,22 @@ func (c *ConfirmBattery) Start(name, webhookURL string, confirmationInterval, no
 		}
 	}()
 
-	for battery := range c.BatteryCharges {
-		c.logger.Infof("現在のバッテリー： %f ％", battery)
-		if battery < notificationCondition {
-			text := fmt.Sprintf("PC：%sのバッテリーが%f以下になりました。現在は%f％です。充電してください！",
-				name, notificationCondition, battery)
-			message := slack.NewMessage(name, text, ":battery:")
-			if err := message.Notify(webhookURL); err != nil {
-				log.Printf("action=send message to slack, err=%v\n", err)
+	for battery := range c.BatteryInfo {
+		if battery.State.String() == "Discharging" {
+			batteryPercentage := (battery.Current / battery.Full) * 100
+			c.logger.Infof("現在のバッテリー： %f ％", batteryPercentage)
+			if batteryPercentage < notificationCondition {
+				text := fmt.Sprintf("PC：%sのバッテリーが%f以下になりました。現在は%f％です。充電してください！",
+					name, notificationCondition, batteryPercentage)
+				message := slack.NewMessage(name, text, ":battery:")
+				if err := message.Notify(webhookURL); err != nil {
+					log.Printf("action=send message to slack, err=%v\n", err)
+				}
 			}
+			wg.Done()
 		}
-		wg.Done()
 	}
 	wg.Wait()
-	close(c.BatteryCharges)
+	close(c.BatteryInfo)
 	c.logger.Info("Close Battery Stored Channel")
 }
